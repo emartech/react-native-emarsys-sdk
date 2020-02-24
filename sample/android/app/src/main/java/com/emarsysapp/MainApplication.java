d08733cee858176f0e273481116a842f1921a007
Tomasz J Góralczyk <1909894+tjg1@users.noreply.github.com>
2020-02-24 13:55:50 +0100

    Enable MultiDex in Android sample app
    Method limit was reached with new version of the SDK

--- sample/android/app/src/main/java/com/emarsysapp/MainApplication.java
+++ sample/android/app/src/main/java/com/emarsysapp/MainApplication.java
@@ -8,10 +8,11 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import androidx.multidex.MultiDexApplication;
 
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends MultiDexApplication implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
