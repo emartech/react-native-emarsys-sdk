3d7f1d4362a322117485c980858ffb6622bbd2dc
biancalui <60601155+biancalui-emarsys@users.noreply.github.com>
2022-07-05 12:13:19 +0800

    fix: setPushToken accepts NSString
    MV-303

--- ios/RNEmarsysWrapper.m
+++ ios/RNEmarsysWrapper.m
@@ -122,9 +122,10 @@ RCT_EXPORT_METHOD(trackCustomEvent:(NSString * _Nonnull)eventName eventAttribute
     }
 }
 
-RCT_EXPORT_METHOD(setPushToken:(NSData * _Nonnull)deviceToken resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject ) {
+RCT_EXPORT_METHOD(setPushToken:(NSString * _Nonnull)deviceToken resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject ) {
     @try {
-        [Emarsys.push setPushToken:deviceToken completionBlock:^(NSError *error) {
+        NSData *pushTokenData = [self dataWithDeviceToken:deviceToken];
+        [Emarsys.push setPushToken:pushTokenData completionBlock:^(NSError *error) {
             if (NULL != error) {
                 reject(@"RNEmarsysWrapper", @"setPushToken: ", error);
             } else {
@@ -135,6 +136,20 @@ RCT_EXPORT_METHOD(setPushToken:(NSData * _Nonnull)deviceToken resolver:(RCTPromi
     @catch (NSException *exception) {
         reject(exception.name, exception.reason, nil);
     }
+}
+
+- (NSData *)dataWithDeviceToken:(NSString *)deviceToken {
+    deviceToken = [deviceToken stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSMutableData *data = [[NSMutableData alloc] init];
+    unsigned char whole_byte;
+    char byte_chars[3] = {'\0','\0','\0'};
+    for (int i = 0; i < ([deviceToken length] / 2); i++) {
+        byte_chars[0] = [deviceToken characterAtIndex:i*2];
+        byte_chars[1] = [deviceToken characterAtIndex:i*2+1];
+        whole_byte = strtol(byte_chars, NULL, 16);
+        [data appendBytes:&whole_byte length:1];
+    }
+    return data;
 }
 
 RCT_EXPORT_METHOD(clearPushToken:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject ) {
@@ -578,7 +593,7 @@ RCT_EXPORT_METHOD(getPushToken:(RCTPromiseResolveBlock)resolve rejecter:(RCTProm
     const char *data = [deviceToken bytes];
     NSMutableString *token = [NSMutableString string];
 
-    for (NSUInteger i = 0; i < [deviceToken length]; i++) {
+    for (int i = 0; i < [deviceToken length]; i++) {
         [token appendFormat:@"%02.2hhX", data[i]];
     }
 
